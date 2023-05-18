734faaab31c719636f7c3240a98b5ce50d32af4f
AlexPst <alexandre-proust.dev@proton.me>
2023-05-18 20:34:39 +0200

    feat : add responsive design for header and footer

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,12 @@
+// Redirection to home page
 function goToAccueil() {
     window.location.href='../../index.html';
+}
+
+// Functions for Hamburger Menus on mobile
+function showHamburgerMenus(){
+    document.getElementsByClassName('hambMenusList')[0].style.display = 'block'
+}
+function hideHamburgerMenus(){
+    document.getElementsByClassName('hambMenusList')[0].style.display = 'none'
 }
